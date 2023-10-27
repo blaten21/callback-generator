76da2927a91dcaabb0cae7ba1609a590f7060d35
Michael Kaiser <blaten21@gmail.com>
2023-10-26 19:21:30 -0700

    Added confirm listener

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const app = express();
 
+// Use the built-in express.json() middleware to parse incoming JSON payloads
+app.use(express.json());
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Set up a POST route at the /confirmation path
+app.post('/confirm', (req, res) => {
+  // Respond with a static JSON template
+  res.json({
+    status: 'success',
+    message: 'What do you want to respond with?'
+    // ... any other key-value pairs you want to send in the template
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
